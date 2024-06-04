456913a71c60c34af2df3d280d0227c73a282bb0
Sangjin Lee <sangjin.lee1012@gmail.com>
2024-06-05 01:56:35 +0900

    Update get_mac-settings.sh

--- scripts/get_mac-settings.sh
+++ scripts/get_mac-settings.sh
@@ -1,11 +1,9 @@
 ##> Only run if ./update_mac-settings.sh needs updating
 
 SETUP_DIR=$HOME/mac_setup
-AUX_DIR=$SETUP_DIR/auxiliary
 
-FRESH_SETTINGS=$AUX_DIR/fresh-mac-settings.txt
-CURRENT_SETTINGS=$AUX_DIR/current-mac-settings.txt
-# MODIFIED_SETTINGS=$AUX_DIR/modified-mac-settings.txt
+FRESH_SETTINGS=$SETUP_DIR/files/fresh-mac-settings.txt
+CURRENT_SETTINGS=$SETUP_DIR/files/current-mac-settings.txt
 
 if [ ! -f $FRESH_SETTINGS ]
 then
